0fb1d9acd18ab51ce211f1c7766a4f87fa8093c3
itzaymvn <aymanbdouzi@gmail.com>
2024-08-14 00:22:48 +0100

    chore: Update sign up and login pages with image and redirection logic

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -2,6 +2,9 @@ import SocialButtons from "@/components/socialauth"
 import Divider from "@/components/ui/divider"
 import { Metadata } from "next"
 import SignInForm from "./form"
+import Image from "next/image"
+import { useAuth } from "@/lib/lucia"
+import { redirect } from "next/navigation"
 
 export const metadata: Metadata = {
 	title: "Sign In - Trimfy",
@@ -9,22 +12,40 @@ export const metadata: Metadata = {
 		"Sign in to Trimfy to access your account and start shortening links.",
 }
 
-const SignIn = ({
+const SignIn = async ({
 	searchParams,
 }: {
 	searchParams: { [key: string]: string | string[] | undefined }
 }) => {
-	return (
-		<div className="flex min-h-screen bg-gray-900 items-center justify-center text-white">
-			<div className="space-y-4">
-				<h1 className="text-5xl font-bold text-center">Sign In</h1>
-				<p className="text-gray-400 text-lg">
-					Sign in to your account using one of the methods below.
-				</p>
+	const { session } = await useAuth()
+	if (session) {
+		redirect("/dashboard")
+	}
 
-				<SocialButtons />
-				<Divider text="OR" />
-				<SignInForm params={searchParams} />
+	return (
+		<div className="flex min-h-screen bg-gray-900 items-start pt-4 justify-center text-white px-4">
+			<div className="flex flex-col lg:flex-row items-start justify-between w-full max-w-5xl lg:space-x-8">
+				<Image
+					src="./thought_process.svg"
+					alt="Trimfy Logo"
+					width="500"
+					height="400"
+					className="rounded-lg object-cover hidden lg:block"
+					style={{
+						aspectRatio: "500/400",
+						objectFit: "contain",
+					}}
+				/>
+				<div className="space-y-4 max-w-md w-full">
+					<h1 className="text-5xl font-bold text-center">Sign In</h1>
+					<p className="text-gray-400 text-lg text-center">
+						Sign in to your account using one of the methods below.
+					</p>
+
+					<SocialButtons />
+					<Divider text="OR" />
+					<SignInForm params={searchParams} />
+				</div>
 			</div>
 		</div>
 	)
--- src/app/(auth)/register/page.tsx
+++ src/app/(auth)/register/page.tsx
@@ -2,6 +2,9 @@ import SocialButtons from "@/components/socialauth"
 import Divider from "@/components/ui/divider"
 import { Metadata } from "next"
 import SignUpForm from "./form"
+import Image from "next/image"
+import { useAuth } from "@/lib/lucia"
+import { redirect } from "next/navigation"
 
 export const metadata: Metadata = {
 	title: "Sign Up - Trimfy",
@@ -9,19 +12,37 @@ export const metadata: Metadata = {
 		"Sign up for Trimfy to access your account and start shortening links.",
 }
 
-const SignUp = () => {
-	return (
-		<div className="flex min-h-screen bg-gray-900 items-center justify-center text-white">
-			<div className="space-y-4">
-				<h1 className="text-5xl font-bold text-center">Sign Up</h1>
-				<p className="text-gray-400 text-lg">
-					Sign up for Trimfy to access your account and start
-					shortening links.
-				</p>
+const SignUp = async () => {
+	const { session } = await useAuth()
+	if (session) {
+		redirect("/dashboard")
+	}
 
-				<SocialButtons />
-				<Divider text="OR" />
-				<SignUpForm />
+	return (
+		<div className="flex min-h-screen bg-gray-900 items-start pt-4 justify-center text-white px-4">
+			<div className="flex flex-col lg:flex-row items-start justify-between w-full max-w-5xl lg:space-x-8">
+				<Image
+					src="./thought_process.svg"
+					alt="Trimfy Logo"
+					width="500"
+					height="400"
+					className="rounded-lg object-cover hidden lg:block"
+					style={{
+						aspectRatio: "500/400",
+						objectFit: "contain",
+					}}
+				/>
+				<div className="space-y-4 max-w-md w-full">
+					<h1 className="text-5xl font-bold text-center">Sign Up</h1>
+					<p className="text-gray-400 text-lg text-center">
+						Sign up for Trimfy to access your account and start
+						shortening links.
+					</p>
+
+					<SocialButtons />
+					<Divider text="OR" />
+					<SignUpForm />
+				</div>
 			</div>
 		</div>
 	)
